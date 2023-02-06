a101190efac5939c6446a64b2b82bbf4e0975e34
edgarrivero33 <108140039+edgarrivero33@users.noreply.github.com>
2023-02-06 09:56:13 -0700

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello Team 5!</h1>
+      <p>Please confirm that you are able to see this team</p>
+    </div>
+  );
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# NAIVE_TEAM5
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-tucrxe)
